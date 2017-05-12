8b0c60b2519ac87012fd4fb5a10c1184682e07b9
jv-lee <jv.lee@foxmail>
2017-05-12 19:21:54 +0800

    update code

--- builder/src/main/java/com/jv/builder/MainActivity.java
+++ builder/src/main/java/com/jv/builder/MainActivity.java
@@ -3,11 +3,28 @@ package com.jv.builder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jv.builder.module.HotDryNoodlesWithBuilder;
+import com.jv.builder.module.Product;
+import com.jv.builder.module.Progremer;
+import com.jv.builder.module.TechManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Product product = new TechManager()
+//                .setAppFunction("")
+//                .setAppName("")
+//                .setAppSystem("")
+//                .build();
+        Product product = new Progremer()
+                .setAppSystem(15)
+                .setAppName("")
+                .setAppFunction("")
+                .build();
+
     }
 }
