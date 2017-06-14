fd3049d08c1195387a2147788b0c540cc2ed0996
李嘉伟 <jv.lee@foxmail.com>
2017-06-14 19:21:04 +0800

    update viewActivity

--- animation/src/main/java/com/jv/animation/activity/ToViewActivity.kt
+++ animation/src/main/java/com/jv/animation/activity/ToViewActivity.kt
@@ -3,7 +3,6 @@ package com.jv.animation.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.view.ViewCompat
-import com.bumptech.glide.Glide
 
 import com.jv.animation.R
 import kotlinx.android.synthetic.main.activity_to_view.*
@@ -14,8 +13,9 @@ class ToViewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_to_view)
 
-        Glide.with(this@ToViewActivity).load("http://ww2.sinaimg.cn/large/7a8aed7bgw1eutscfcqtcj20dw0i0q4l.jpg").into(iv_view)
-        ViewCompat.setTransitionName(iv_view,"sharedImage")
+        iv_view.setImageDrawable(ViewActivity.drawable)
+//        Glide.with(this@ToViewActivity).load("http://ww2.sinaimg.cn/large/7a8aed7bgw1eutscfcqtcj20dw0i0q4l.jpg").into(iv_view)
+        ViewCompat.setTransitionName(iv_view, "sharedImage")
 
     }
 }
--- animation/src/main/java/com/jv/animation/activity/ViewActivity.kt
+++ animation/src/main/java/com/jv/animation/activity/ViewActivity.kt
@@ -1,6 +1,7 @@
 package com.jv.animation.activity
 
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -14,6 +15,10 @@ import kotlinx.android.synthetic.main.activity_view.*
 
 class ViewActivity : AppCompatActivity() {
 
+    companion object {
+        var drawable: Drawable? = null
+    }
+
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +26,7 @@ class ViewActivity : AppCompatActivity() {
         Glide.with(this@ViewActivity).load("http://ww2.sinaimg.cn/large/7a8aed7bgw1eutscfcqtcj20dw0i0q4l.jpg").into(iv_view)
         iv_view.setOnClickListener({
             val options = ActivityOptionsCompat.makeSceneTransitionAnimation(this, iv_view, "sharedImage")
+            drawable = iv_view.drawable
             ActivityCompat.startActivity(this@ViewActivity, Intent(this, ToViewActivity::class.java), options.toBundle())
         })
 
